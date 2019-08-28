1bb7578b8b452f3381e898162074486a8484d818
Matheus Olimpio de Oliveira <mah.oliver_crz@hotmail.com>
2019-08-28 19:31:43 -0300

    Update homeController.php

--- src/Controllers/homeController.php
+++ src/Controllers/homeController.php
@@ -2,21 +2,10 @@
 class homeController extends controller{
     public function __construct(){
         parent::__construct();
-        
-        $user = new usuarios();
-        if(!$user->logged()){
-            header("Location: /mvc/login");
-            echo "<script>console.log('Voce não tem permissão para acessar o sistema');</script>";
-        }
     }
 
     public function index(){
-        $usuario = new Usuarios();
-        $dados = [
-            "id" => $_SESSION['twlg']['id'],
-            "nome" => $_SESSION['twlg']['nome'],
-            "email" => $_SESSION['twlg']['email']
-        ];
+       
         $this->loadTemplate('home', $dados);
     }
 
